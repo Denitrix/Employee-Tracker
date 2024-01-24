f8ac84b3af53796b4e3dcbc2354f87c03b827f5b
Alex Majorowicz <alexmajors0613@gmail.com>
2024-01-24 04:42:42 -0600

    add viewRoles

--- server.js
+++ server.js
@@ -84,6 +84,7 @@ const menu = () => {
         case "Update Employee Role":
           break;
         case "View All Roles":
+          viewRoles();
           break;
         case "Add a Role":
           break;
@@ -113,6 +114,20 @@ const viewEmployees = async () => {
     LEFT JOIN role r ON e.role_id = r.id
     JOIN  department d ON r.department_id = d.id
     LEFT JOIN employee AS m ON e.manager_id = m.id`
+  );
+  console.table(rows);
+  menu();
+};
+
+const viewRoles = async () => {
+  const [rows, fields] = await db.execute(
+    `SELECT
+    r.id AS ID, 
+    r.title AS Title, 
+    r.salary AS Salary, 
+    d.name AS Department 
+    FROM role AS r
+    JOIN department d ON r.department_id = d.id`
   );
   console.table(rows);
   menu();
