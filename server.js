371338249cba78a518642e86d07d94dddec5d6ee
Alex Majorowicz <alexmajors0613@gmail.com>
2024-01-24 02:45:31 -0600

    change to mysqul2/promise

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require("express");
+/* const express = require("express"); */
 // Import and require mysql2
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 const inquirer = require("inquirer");
 
-const PORT = process.env.PORT || 3001;
+/* const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
@@ -11,17 +11,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Connect to database
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    // MySQL username,
-    user: "root",
-    // MySQL password
-    password: "",
-    database: "employee_db",
-  },
-  console.log(`Connected to the employee_db database.`)
-);
+let db;
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+}); */
+
+const init = async () => {
+  db = await mysql.createConnection(
+    {
+      host: "localhost",
+      // MySQL username,
+      user: "root",
+      // MySQL password
+      password: "",
+      database: "employee_db",
+    },
+    console.log(`Connected to the employee_db database.`)
+  );
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
